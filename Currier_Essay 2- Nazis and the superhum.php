24e341070d15f187dd462ad9c6fe5e4dc2d4ce81
abbycurr <61121832+abbycurr@users.noreply.github.com>
2020-02-29 17:25:22 -0500

    Add files via upload

---
+++ Currier_Essay 2- Nazis and the superhum.php
@@ -0,0 +1,4 @@
+Currier_Essay 2- Nazis and the superhuman
+Victor's obsession with creating a being superior to humans
+Nazi experimentations during the 1930s and 1940s with improvements to the aryan race
+Both focused on creating the next class of human beings
