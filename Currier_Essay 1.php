ad7813077a78b21ec26a4d72fc231c11924f6f84
abbycurr <61121832+abbycurr@users.noreply.github.com>
2020-02-29 16:57:57 -0500

    Update Currier_Essay 1.php

--- Currier_Essay 1.php
+++ Currier_Essay 1.php
@@ -1,1 +1,5 @@
 Currier_Essay 1
+## Gender roles and the assumption that all women require a marital partner
+Safie and her marriage to Felix
+Elizabeth and her marriage to Victor
+Margaret and her anonymous husband
